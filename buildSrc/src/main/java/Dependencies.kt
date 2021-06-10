b2b3a551fd17fc65fdd117f83ca9ae5ec6fb6cad
정상현 <tkdgusl94@mailplug.co.kr>
2021-06-10 14:42:32 +0900

    gradle 파일 정리2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,10 +1,45 @@
-object Libraries {
-    private object Versions {
-        const val coreKtx = "1.5.0"
-        const val appCompat = "1.3.0"
-    }
+object Versions {
+    const val KOTLIN_VERSION = "1.5.10"
+    const val KOTLINX_COROUTINES = "1.5.0"
 
-    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
-    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val CORE_KTX = "1.5.0"
+    const val APP_COMPAT = "1.3.0"
+    const val ACTIVITY_KTX = "1.2.3"
+    const val FRAGMENT_KTX = "1.3.4"
+    const val LIFECYCLE_VIEWMODEL_KTX = "2.3.1"
+    const val HILT = "2.35.1"
+    const val MATERIAL = "1.3.0"
+
+    const val JUNIT = "4.13.2"
+    const val ANDROID_JUNIT = "1.1.2"
+    const val ESPRESSO_CORE = "3.3.0"
 }
 
+object Kotlin {
+    const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.KOTLIN_VERSION}"
+    const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLINX_COROUTINES}"
+    const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.KOTLINX_COROUTINES}"
+}
+
+object AndroidX {
+    const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:${Versions.APP_COMPAT}"
+
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:${Versions.ACTIVITY_KTX}"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_KTX}"
+
+    const val LIFECYCLE_VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.LIFECYCLE_VIEWMODEL_KTX}"
+}
+
+object Google {
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+
+    const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+}
+
+object Test {
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val ANDROID_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE}"
+}
